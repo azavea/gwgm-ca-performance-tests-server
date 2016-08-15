164677fa2e21c914c651ad8ecf1af32f0f905db8
Nathan Zimmerman <npzimmerman@gmail.com>
2016-08-15 16:16:35 -0400

    Break up route definitions

--- src/main/scala/geotrellis/analysis/Routes.scala
+++ src/main/scala/geotrellis/analysis/Routes.scala
@@ -3,9 +3,12 @@ package geotrellis.analysis
 import akka.http.scaladsl.server.Directives._
 
 import geotrellis.analysis.status._
+import geotrellis.analysis.geomesa.connection._
+import geotrellis.analysis.geowave.connection._
 
 object Routes {
-  def apply() =
+
+  val systemRoutes =
     pathPrefix("system") {
       pathPrefix("status") {
         pathEndOrSingleSlash {
@@ -14,47 +17,54 @@ object Routes {
           }
         }
       }
-    } ~
+    }
+
+  val geomesaRoutes =
     pathPrefix("geomesa") {
-      pathPrefix("status") {
-        parameters('table) { table =>
+      pathPrefix(Segment) { tableName =>
+        pathPrefix("status") {
           get {
-            StatusService.geomesa(table)
+            StatusService.geomesa(tableName)
           }
-        }
-      } ~
+        } ~
         pathPrefix("sfts") {
           pathEndOrSingleSlash {
             get {
-              geomesa.SimpleFeatureTypes.list
+              geomesa.SimpleFeatureTypes.list(tableName)
             }
           } ~
           pathPrefix(Segment) { sftName =>
-            get {
-              geomesa.SimpleFeatureTypes.detail(sftName)
-            }
-          }
-        }
-    } ~
-    pathPrefix("geowave") {
-      pathPrefix("status") {
-        parameters('table) { table =>
-          get {
-            StatusService.geowave(table)
-          }
-        }
-      } ~
-      pathPrefix("sfts") {
-        pathEndOrSingleSlash {
-          get {
-            geowave.SimpleFeatureTypes.list
-          }
-        } ~
-        pathPrefix(Segment) { sftName =>
-          get {
-            geowave.SimpleFeatureTypes.detail(sftName)
+           get {
+             geomesa.SimpleFeatureTypes.detail(tableName, sftName)
+           }
           }
         }
       }
     }
+
+  //val geowaveRoutes =
+  //  pathPrefix("geowave") {
+  //    pathPrefix("status") {
+  //      parameters('table) { table =>
+  //        get {
+  //          StatusService.geowave(table)
+  //        }
+  //      }
+  //    } ~
+  //    pathPrefix("sfts") {
+  //      pathEndOrSingleSlash {
+  //        get {
+  //          geowave.SimpleFeatureTypes.list
+  //        }
+  //      } ~
+  //      pathPrefix(Segment) { sftName =>
+  //        get {
+  //          geowave.SimpleFeatureTypes.detail(sftName)
+  //        }
+  //      }
+  //    }
+  //  }
+
+  def apply() =
+    systemRoutes ~ geomesaRoutes
 }
